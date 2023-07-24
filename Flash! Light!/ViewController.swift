59d6b99e6a3c5d68bb6b7f6f92b22d53abd4641c
Sarah Strickley <sarahstrickley@cwi.edu>
2023-07-24 14:58:07 -0500

    App finished with counter cycling through multiple colors.

--- Flash! Light!/ViewController.swift
+++ Flash! Light!/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - IB Outlets
+    
+    @IBOutlet weak var lightButton: UIButton!
+    
+    //MARK: - Instance Properties
+    var lightOn = true
+    var counter = 1
+    
+    //MARK: - VDL
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        updateUI()
+        
     }
-
-
+    
+    //MARK: - IB Actions
+    
+    @IBAction func lightButtonTapped(_ sender: Any) {
+        //lightOn = !lightOn
+        //lightOn.toggle()
+      
+        counter += 1
+        updateUI()
+        
+    }
+    
+    
+    //MARK - Instance Methods
+    fileprivate func updateUI() {
+        
+        if lightOn {
+            if counter.isMultiple(of: 2) {
+                view.backgroundColor = .white
+            } else if counter.isMultiple(of: 3) {
+                view.backgroundColor = .red
+            } else if counter.isMultiple(of: 5) {
+                view.backgroundColor = .cyan
+            } else if counter.isMultiple(of: 7){
+                view.backgroundColor = .magenta
+            } else {
+                view.backgroundColor = .purple
+            }
+            
+            //
+            //            view.backgroundColor = .white
+            ////            lightButton.setTitle("Off", for: .normal)
+            ////            lightButton.setTitleColor(.black, for: .normal)
+            //        } else {
+            //
+            //            view.backgroundColor = .black
+            ////            lightButton.setTitle("On", for: .normal)
+            ////            lightButton.setTitleColor(.white, for: .normal)
+            //        }
+        }
+    }
 }
-
